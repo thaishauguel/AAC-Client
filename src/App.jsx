41158ff59222843999d8c7d8cf36bb282f8c402c
Magali <fort_magali@hotmail.fr>
2021-03-28 18:16:02 +0200

    add Artist page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
+import Artist from "./pages/Artist";
 // import MyActivity from "./pages/SubProfilePages/MyActivity";
 // import MyCollection from "./pages/SubProfilePages/MyCollection";
 // import MyCreactions from "./pages/SubProfilePages/MyCreactions";
@@ -60,16 +61,12 @@ export default class App extends Component {
         />
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route
-            exact
-            path="/results"
-            render={() => (
-              <Results
-                searchedValue={this.state.searchedValue}
-              />
+          <Route exact path="/results" render={() => (
+              <Results searchedValue={this.state.searchedValue}/>
             )}
           />
           <Route exact path="/artworks/:id" component={OneArtwork} />
+          <Route exact path="/artist/:id" component={Artist} />
           <Route exact path="/signin" component={Signin} />
           <Route exact path="/signup" component={Signup} />
           <ProtectedRoute exact path="/profile" component={Profile} />
